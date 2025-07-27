836aed8da67383df066d9923d209261e9c453875
MaxZ <maxzonoff@gmail.com>
2025-07-27 23:05:17 +0300

    добавлена авторизация

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-from src.queries import insert_user
+from src.queries import insert_user, get_user_id, get_username
 import psycopg2
 
 def main():
+    auth_user_id = None
+
     while True:
-        print('1. Войти')
-        print('2. Зарегистрироваться')
-        print('3. Выйти')
+        if auth_user_id is not None:
+            username = get_username(auth_user_id)
+            print(f'Добро пожаловать {username}')
+        if auth_user_id is None:
+            print('1. Войти')
+            print('2. Зарегистрироваться')
+        else:
+            print('3. Выйти')
+            print('4. Получить вопрос ')
+        print('5. Завершить программу ')
         user = input()
         match user:
             case '1':
-                print('Войти')
+                if auth_user_id is not None:
+                    print('Вы ввели не верный пункт меню ')
+                    continue
+                username = input('Введите логин: ')
+                password = input('Введите пароль: ')
+                user_id = get_user_id(username, password)
+                if user_id is None:
+                    print('Неверный логин или пароль ')
+                else:
+                    print('Пользователь авторизован ')
+                    auth_user_id = user_id
             case '2':
+                if auth_user_id is not None:
+                    print('Вы ввели не верный пункт меню ')
+                    continue
                 username = input('Введите логин: ')
                 password = input('Введите пароль: ')
                 try:
@@ -22,10 +44,22 @@ def main():
                 else:
                     print(f'{username} создан!')
             case '3':
+                if auth_user_id is None:
+                    print('Вы ввели не верный пункт меню ')
+                    continue
+                auth_user_id = None
+                print('Вы вышли из аккаунта ')
+            case '4':
+                if auth_user_id is None:
+                    print('Вы ввели не верный пункт меню ')
+                    continue
+                print('Что такое subscriptable?')
+            case '5':
                 print('Программа завершена. ')
                 return
             case _:
                 print('Не верный пункт меню. ')
+
 
 if __name__ == '__main__':
     main()
--- src/queries.py
+++ src/queries.py
@@ -8,3 +8,27 @@ def insert_user(username: str, password: str) -> None:
                 "INSERT INTO users (username, password) VALUES (%s, %s)",
                 (username, password)
             )
+
+
+def get_user_id(username: str, password: str) -> int | None:
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT id FROM users WHERE username=%s AND password=%s",
+                (username, password),
+            )
+            result = cur.fetchone()
+            if result is None:
+                return None
+            return result[0]
+
+
+def get_username(user_id: int) -> str:
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT username FROM users WHERE id=%s",
+                (user_id,),
+            )
+            result = cur.fetchone()
+            return result[0]
